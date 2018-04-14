cbb35eb5cf17d659ec1fe74824ab3b670cd2bdf8
TomekFerens <feeroo@gmail.com>
2018-04-14 13:04:16 +0200

    Added dependencies to prevent build failure

--- package/dms/dms.mk
+++ package/dms/dms.mk
@@ -9,4 +9,7 @@ DMS_SITE = $(call github,ferot,dms,master)
 DMS_INSTALL_STAGING = YES
 DMS_INSTALL_TARGET = YES
 DMS_SUPPORTS_IN_SOURCE_BUILD = NO
+
+DMS_DEPENDENCIES = qt5base opencv3 fann jsoncpp paho-mqtt-c log4c sqlite_moderncpp
+
 $(eval $(cmake-package))
